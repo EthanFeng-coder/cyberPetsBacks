4339b58d362f1f3dc6704984e8798cbe8cdb42a4
imp-wq <2662727613@qq.com>
2024-08-01 22:43:07 +1000

    finished gradio

--- gradio/front-end.py
+++ gradio/front-end.py
@@ -5,7 +5,12 @@ def greet(your_input):
     return "Hello " + your_input + "!"
 
 
-demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
-
 if __name__ == "__main__":
+    with gr.Blocks() as demo:
+        video_output = gr.Video(label="Processed Image")
+        audio_input = gr.Audio(type="numpy", label="Input", autoplay=True)
+        audio_output = gr.Audio(type="filepath", label="output", value='../Recording.m4a')
+        submit_button = gr.Button("Submit").click(
+            inputs=[audio_input],
+            outputs=[video_output, audio_output], )
     demo.launch(share=True)
